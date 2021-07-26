d55f843becadc6bc30cedb210f67313e5638fb52
Matheus Lucas <nerdola.cartola@gmail.com>
2021-07-26 20:18:19 -0300

    Starting Next and React project

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from '../src/components/Home'
+
+
+export default function Index(){
+    return(
+        <Home>
+        </Home>
+    )
+}
+
+
+
